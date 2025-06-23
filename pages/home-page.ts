94df4b83ed75d418461f2a910b098b64762deb19
W4veN <karolwajs55@gmail.com>
2025-06-23 19:00:32 +0200

    first test for financials ection

--- pages/home-page.ts
+++ pages/home-page.ts
@@ -1,11 +1,20 @@
 import { type Page, type Locator } from '@playwright/test'
 export class HomePage {
     readonly page: Page
-    readonly financialServicesSection: Locator
+    readonly financialServicesArticleSection: Locator
+    readonly bankingColumn: Locator
+    readonly financialServicesContainer: Locator
     constructor(page: Page) {
         this.page = page
-        this.financialServicesSection = page
-            .getByRole('heading', { name: 'End-to-end solutions for' })
-            .describe('End-to-end solutions for financial services Header')
+        this.financialServicesContainer = page
+            .locator('.container-fluid')
+            .describe('Financial Services Container')
+        this.financialServicesArticleSection = page
+            .getByRole('article')
+            .filter({ hasText: 'End-to-end solutions for financial services' })
+            .describe('End-to-end solutions for financial services article')
+        this.bankingColumn = page
+            .locator('div > a[aria-label="Learn more"]')
+            .describe('Banking Column Section')
     }
 }
--- tests/mobile/financial-services.spec.ts
+++ tests/mobile/financial-services.spec.ts
@@ -1,12 +1,37 @@
 import { test, expect } from '@playwright/test'
 import { HomePage } from '../../pages/home-page'
-
-test('has heading', async ({ page }) => {
+test('End-to-end solutions for financial services section', async ({
+    page,
+}) => {
     await page.goto('/')
+
     const homePage = new HomePage(page)
-    // Expect a title "to contain" a substring.
     await expect(
-        homePage.financialServicesSection,
+        homePage.financialServicesArticleSection,
         'Check if financial services section is visible'
     ).toBeVisible()
+    await expect(
+        homePage.financialServicesArticleSection,
+        'Financial Services section should contain proper data'
+    ).toMatchAriaSnapshot(`- article:
+  - heading "End-to-end solutions for financial services" [level=2]
+  - link "Learn more":
+    - /url: https://www.sapfioneer.com/banking/
+    - figure
+    - heading "Banking" [level=3]
+    - paragraph: Reach more customers and build trust with real-time financial insights and risk control. Join the 800 banks globally who have chosen our platform.
+    - text: Learn more 
+  - link "Learn more":
+    - /url: https://www.sapfioneer.com/insurance/
+    - figure
+    - heading "Insurance" [level=3]
+    - paragraph: Accelerate digital insurance processes to meet customer expectations and create new revenue streams by connecting easily to the ecosystem.
+    - text: Learn more 
+  - link "Learn more":
+    - /url: https://www.sapfioneer.com/finance-esg/
+    - figure
+    - heading "Finance & ESG" [level=3]
+    - paragraph: Establish a single source of truth for profitability and turn compliance into your advantage using our integrated finance & ESG platform.
+    - text: Learn more `)
+    await page.close()
 })
